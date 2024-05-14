fc2c2af26ecee01f1ffde17bcf4d7106ae78b1e4
soyceros <soyceros@protonmail.com>
2024-05-13 19:40:09 -0600

    Corrigiendo

--- variables.js
+++
@@ -1,2 +0,0 @@
-# JavaScript-Basico
-¡Bienvenidos! Este repositorio está diseñado para ayudarte a aprender los conceptos básicos de JavaScript. Aquí encontrarás ejemplos y ejercicios prácticos para empezar a programar.
